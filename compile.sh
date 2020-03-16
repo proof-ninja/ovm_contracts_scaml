7c41a2461424379f1447f14167a3bb8351816a92
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2020-03-16 17:36:37 +0900

    types/iterable_types in SCaml

--- compile.sh
+++ compile.sh
@@ -3,6 +3,7 @@ set -eux
 FILE=ovm.ml
 DEPENDENCIES="lib.ml \
               types/ovm_primitive_types.ml \
+              types/ovm_iterable_types.ml \
               entry.ml
 "
 cat $DEPENDENCIES > $FILE
---
+++ types/ovm_iterable_types.ml
@@ -0,0 +1,4 @@
+type checkpoint_id = bytes
+type claim_id = string
+type checkpoints = (checkpoint_id, checkpoint) map
+type commitments = (nat, bytes) map
