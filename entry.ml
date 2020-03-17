6eece11f6a37fe42fee386951346debdea5f60c8
Yoshihiro Imai <yoshihiro503@proofcafe.org>
2020-03-17 17:07:13 +0900

    entry point

--- entry.ml
+++ entry.ml
@@ -1,1 +1,8 @@
-let main () () = ([], ())
+let main (action: action)(s: ovm_storage) : context =
+  match action with
+    | Deposit(deposit_params) -> deposit_action deposit_params s
+    | FinalizeCheckpoint(finalize_checkpoint_params) -> finalize_checkpoint_action finalize_checkpoint_params s
+    | FinalizeExit(finalize_exit_params) ->
+      finalize_exit_action finalize_exit_params s
+    | Submit(submit_params) -> submit_action submit_params s
+     | ClaimProperty(claim_property_params) -> claim_property_action claim_property_params s
